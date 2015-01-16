ff2983d2bef47d61006b55a93ca254bfaa851bb1
letrungson1@gmail.com <letrungson1@gmail.com@1daa7095-eb54-720b-69af-868225a4c872>
2015-01-16 05:38:10 +0000

    some working with pointers and void pointers

--- trunk/Cpp/Cpp Operator/main.cpp
+++ trunk/Cpp/Cpp Operator/main.cpp
@@ -7,73 +7,25 @@
 using namespace std;
 
 
-class Person
-{
-	
-	
-	public:
-
-	Person()
-	{
-	};
-	
-	Person(double height_in, double income_in)
-	{
-		height = height_in;
-		income = income_in;
-	}
-	
-	
-	~Person()
-	{
-	
-	}
-	
-	double getHeight()
-	{
-		return height;
-	}
-
-	double getIncome()
-	{
-		return income;
-	}
-	
-	Person operator+(const Person& B)
-	{
-		Person ret;
-		
-		// why use this but not height??
-		//ret.height = (this->height + B.height)/2;
-		//ret.income = this->income + B.income;
-		ret.height = (height + B.height)/2;
-		ret.income = income + B.income;
-
-
-		return ret;
-	}
-	
-	
-	private:
-	
-	double height, income;
-
-}; 
-
-
 /** @function main */
 int main( int argc, char** argv )
 {
-	Person A(172, 20000);
-	Person B(145, 5000);
-	
-	// the grown up kid with inheritance
-	Person C = A + B;
-	
-	printf("Result is\n");
-	printf("%.2f\n",C.getHeight());
-	printf("%.2f",C.getIncome());
+
+	void *vptr;
+
+	// void pointer cannot be used like this
+	vptr = "abcd";
+	int i = 103;
+	vptr = &i;
+	cout << vptr << endl;
 	getchar();
-	
+
+	// void pointer cannot be used like this
+	// 1st cast (int *) casts void* to int*
+	// 2nd * is value of pointee that the int* pointer is pointing too
+	cout << *(int *)vptr << endl;
+
+	getchar();
+
 	return 0;
 }
