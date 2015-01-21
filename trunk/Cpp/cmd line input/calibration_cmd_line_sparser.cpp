417a6ad11c8752a6c901328af062c362f79793be
letrungson1 <letrungson1@1daa7095-eb54-720b-69af-868225a4c872>
2015-01-21 10:02:24 +0000

    cmd line input of arguments

---
+++ trunk/Cpp/cmd line input/calibration_cmd_line_sparser.cpp
@@ -0,0 +1,19 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <iostream>
+
+using namespace std;
+
+
+int main( int argc, char** argv )
+{
+
+	cout << argc << endl;
+	cout << argv[0] << endl;
+	getchar();
+
+    return 0;
+}
