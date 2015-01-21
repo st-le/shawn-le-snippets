27ab7a4237143577d8e7fc975a7cf36974c9ea86
letrungson1 <letrungson1@1daa7095-eb54-720b-69af-868225a4c872>
2015-01-21 06:23:51 +0000

    how about passing function from outside class into class function pointer??
    not successful yet!!

--- trunk/Cpp/class member fun pointer/main.cpp
+++ trunk/Cpp/class member fun pointer/main.cpp
@@ -6,6 +6,14 @@
 
 using namespace std;
 
+typedef int (*FunPtrCall)(int a, int b);
+
+int ExtCompute(int a, int b)
+{
+	return (a+b);
+}
+
+
 class myClass
 {
 public: 
@@ -36,12 +44,53 @@ private:
 };
 
 
+class my2ndClass
+{
+public: 
+
+	typedef int (my2ndClass::*calc)(int a, int b);
+
+	calc funptr;
+	FunPtrCall funextptr;
+
+	void set_ext_ptr(FunPtrCall ptr)
+	{
+		funextptr = ptr;
+	}
+
+	void set_cb_ptr(calc ptr)
+	{
+		funptr = ptr;
+	}
+
+	void call_cb_func()
+	{
+		int c = (this->*funptr)(100,300);
+		cout << c << endl;
+		getchar();
+	}
+
+	void call_ext_func()
+	{
+		int c = (this->*funextptr)(100,300);
+		cout << c << endl;
+		getchar();
+	}
+
+private:
+
+};
+
+
 int main()
 {
-	myClass mc;
+	my2ndClass mc;
 
-	mc.set_cb_ptr(&myClass::compute);
-	mc.call_cb_func();
+	//mc.set_cb_ptr(&ExtCompute);
+	//mc.call_cb_func();
+
+	mc.set_ext_ptr(&ExtCompute);
+	mc.call_ext_func();
 
 	return 0;
 }
