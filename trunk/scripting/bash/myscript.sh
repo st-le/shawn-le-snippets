bdefbc85dcb6d8c73dc2d4f3a2de25302fe9e221
letrungson1 <letrungson1@1daa7095-eb54-720b-69af-868225a4c872>
2015-02-04 17:42:42 +0000

    bash script

---
+++ trunk/scripting/bash/myscript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Input reference height [pixel] (default=457.9):"
+read pxl_h 
+echo "Input reference width [pixel] (default=902.4):"
+read pxl_w
+echo "Input target area (default=500):"
+read area
+echo "Input size (height/width) tolerance (default=20):"
+read size_tol 
+echo "Input programm mode [DEBUG_INFO/RESULT_INFO/SCREEN_INFO]:" 
+read OPER_MODE 
+
+
+
